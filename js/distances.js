ed592dc95782a17542424ce10c9fee1209bd544a
Oliver <oliver.hilferty@rezonence.com>
2019-03-09 19:26:39 +0000

    Add distance units

---
+++ js/distances.js
@@ -0,0 +1,17 @@
+// Distances are given in metres (m)
+
+export const metres = distance => distance;
+export const inches = distance => distance / 39.37;
+
+export const units = [
+    {
+        unit: "blue whales",
+        desc: "An average female blue whale",
+        val: 25
+    },
+    {
+        unit: "Golden Gate Bridges",
+        desc: "The Golden Gate Bridge",
+        val: 2737
+    }
+];
