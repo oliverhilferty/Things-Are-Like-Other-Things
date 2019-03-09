40a38ea27347ed3758e2c38eb58fb7608d28afc5
Oliver <oliver.hilferty@rezonence.com>
2019-03-09 11:46:01 +0000

    Add custom select component

--- js/volumes.js
+++ js/volumes.js
@@ -1,8 +1,8 @@
 // Volumes are given in Litres (L)
 
-export const impOunces = amount => amount / 35.195;
-export const impPints = amount => amount / 1.76;
-export const millilires = amount => amount / 1000;
+export const impOunces = volume => volume / 35.195;
+export const impPints = volume => volume / 1.76;
+export const millilires = volume => volume / 1000;
 
 export const units = [
     {
