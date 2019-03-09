3d7176d6710d0a8f62843d01e8cb6759ebdd8520
Oliver <oliver.hilferty@rezonence.com>
2019-03-09 14:08:23 +0000

    Add standardiser function

--- js/app.js
+++ js/app.js
@@ -9,8 +9,20 @@ const getUnit = () => {
     return document.querySelector("select").value;
 };
 
+const standardiseWeight = (weight, unit) => {
+    switch (unit) {
+        case "pounds":
+            return weights.pounds(weight);
+        case "tonnes":
+            return weights.tonnes(weight);
+        case "stone":
+            return weights.stone(weight);
+    }
+};
+
 const button = document.querySelector(".calculate");
 button.addEventListener("click", () => {
-   console.log(getAmount(), getUnit());
+    // console.log(getAmount(), getUnit());
+    console.log(standardiseWeight(getAmount(), getUnit()));
 });
 
